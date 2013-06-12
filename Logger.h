6c22666f5c65546e080772348b6c199bd0510eb2
Russ Johnston <rustyford@gmail.com>
2013-06-12 14:15:38 -0700

    added GPS and odometery logging

--- Logger.h
+++ Logger.h
@@ -109,6 +109,80 @@ public:
     Serial.print("*");
     Serial.println(count);
   }
+  
+  static void logKalman (Gps *gps, AvcCompass *compass, AvcMpu6000 *mpu, float speed) {
+    int count = 0;
+    count += Serial.print("GCM,");
+    count += Serial.print(gps->getLatitude());
+    count += Serial.print(",");
+    count += Serial.print(gps->getLongitude());
+    count += Serial.print(",");
+    count += Serial.print(gps->getHdop());
+    count += Serial.print(",");
+    count += Serial.print(gps->getDistanceTraveled());
+    count += Serial.print(",");
+    count += Serial.print(gps->getFixTime() * 10);
+    count += Serial.print(",");
+    count += Serial.print(gps->getSpeed(), 7);
+    count += Serial.print(",");
+    count += Serial.print(gps->hasWaasLock());
+    count += Serial.print(",");
+    count += Serial.print(compass->getHeading());
+    count += Serial.print(",");
+    count += Serial.print(compass->getXOffset());
+    count += Serial.print(",");
+    count += Serial.print(compass->getYOffset());
+    count += Serial.print(",");
+    count += Serial.print(compass->getZOffset());
+    count += Serial.print(",");
+    count += Serial.print(mpu->getAccelX());
+    count += Serial.print(",");
+    count += Serial.print(mpu->getAccelY());
+    count += Serial.print(",");
+    count += Serial.print(mpu->getAccelZ());
+    count += Serial.print(",");
+    count += Serial.print(mpu->getGyroX());
+    count += Serial.print(",");
+    count += Serial.print(mpu->getGyroY());
+    count += Serial.print(",");
+    count += Serial.print(mpu->getGyroZ());
+    count += Serial.print(",");
+    count += Serial.print(mpu->getTemp());
+    count += Serial.print(",");
+    count += Serial.print(speed);
+    Serial.print("*");
+    Serial.println(count);
+  }
+
+  static void logKalman (AvcCompass *compass, AvcMpu6000 *mpu, float speed) {
+    int count = 0;
+    count += Serial.print("CM,");
+    count += Serial.print(compass->getHeading());
+    count += Serial.print(",");
+    count += Serial.print(compass->getXOffset());
+    count += Serial.print(",");
+    count += Serial.print(compass->getYOffset());
+    count += Serial.print(",");
+    count += Serial.print(compass->getZOffset());
+    count += Serial.print(",");
+    count += Serial.print(mpu->getAccelX());
+    count += Serial.print(",");
+    count += Serial.print(mpu->getAccelY());
+    count += Serial.print(",");
+    count += Serial.print(mpu->getAccelZ());
+    count += Serial.print(",");
+    count += Serial.print(mpu->getGyroX());
+    count += Serial.print(",");
+    count += Serial.print(mpu->getGyroY());
+    count += Serial.print(",");
+    count += Serial.print(mpu->getGyroZ());
+    count += Serial.print(",");
+    count += Serial.print(mpu->getTemp());
+    count += Serial.print(",");
+    count += Serial.print(speed);
+    Serial.print("*");
+    Serial.println(count);
+  }
 };
 
 #endif
